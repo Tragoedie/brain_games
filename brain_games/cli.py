e8b61c206df58c2f2688d1e68d7308d1a3d9e728
Tragoedia <tragoedia@mail.ru>
2021-07-04 21:09:34 +0600

    step_one

---
+++ brain_games/cli.py
@@ -0,0 +1,8 @@
+"""Ask user name, and greeting user."""
+import prompt
+
+
+def welcome_user():
+    """Ask user name, and greeting user."""
+    name = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(name))
