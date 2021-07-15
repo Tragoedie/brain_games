6f5bbfbf8b202f596467a5d2d89e1c49f95ebc87
Tragoedia <tragoedia@mail.ru>
2021-07-16 03:53:45 +0700

    update_engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -35,32 +35,15 @@ def is_win_or_not(name_user, counter, correct_answer, user_answer):
         counter (int): counter of correct given answers.
 
     Returns:
-        bool: is game finished?
+        bool: is game finished? and counter
     """
-    right_answer = is_right_answer(name_user, correct_answer, user_answer)
-    if not right_answer:
-        return True, counter
-    elif counter < 2:
-        return False, counter + 1
-    print(f'Congratulations, {name_user}!')
-    return True, counter
-
-
-def is_right_answer(name_user, corr_answer, user_answer):
-    """Check if user answer is correct.
-
-    Parameters:
-        name_user (string): user name.
-        corr_answer (string): correct answer, calculated by game.
-        user_answer (string): user's answer.
-
-    Returns:
-        bool: is answer correct?
-    """
-    if corr_answer == user_answer:
+    if correct_answer == user_answer and counter < 2:
         print('Correct!')
-        return True
-    print(f'"{user_answer}" is wrong answer ;(.', end='')
-    print(f'Correct answer was "{corr_answer}".')
-    print(f"Let's try again, {name_user}!")
-    return False
+        return False, counter + 1
+    elif correct_answer == user_answer and counter == 2:
+        print(f'Congratulations, {name_user}!')
+    else:
+        print(f'"{user_answer}" is wrong answer ;(.', end='')
+        print(f'Correct answer was "{correct_answer}".')
+        print(f"Let's try again, {name_user}!")
+    return True, counter
